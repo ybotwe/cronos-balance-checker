1bb869e184dde18d15798c436861335bffd049de
Yaw Botwe <ybotwe@gmail.com>
2024-04-27 10:52:21 +0000

    added get CRO balance functionality

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import express, { Request, Response } from "express";
+import { ethers } from "ethers";
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+//Set up ether providers
+const mainnetProvider = new ethers.JsonRpcProvider(process.env.CRONOS_MAIN_RPC);
+const testnetProvider = new ethers.JsonRpcProvider(process.env.CRONOS_TEST_RPC);
+
+app.get("/balance/:network/:address", async (req, res) => {
+  const { network, address } = req.params;
+  try {
+    let balance;
+    if (network === "testnet") {
+      balance = await testnetProvider.getBalance(address);
+    } else if (network === "mainnet") {
+      balance = await mainnetProvider.getBalance(address);
+    } else {
+      return res.status(400).send("Invalid network specified");
+    }
+
+    res.send({
+      address: address,
+      network: network,
+      balance: ethers.formatEther(balance) + " CRO",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching balance");
+  }
 });
 
 app.listen(PORT, () => {
